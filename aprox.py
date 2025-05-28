bc87196ad7eeaabd64bc5f31fade11d85da248fa
zuzia <zuzannabrzeczek71@gmail.com>
2025-05-29 00:22:47 +0200

    setup + dane

---
+++ aprox.py
@@ -0,0 +1,17 @@
+class aprox:
+
+
+    def __init__(self):
+        pass
+
+    def lagrange():
+        pass
+
+    def third():
+        pass
+
+    def analysis():
+        pass
+
+    def graph():
+        pass
